ba52cc191b1ae9e86340c82ce0957ae042efe478
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2023-03-27 09:18:51 +0200

    .

--- tests/device/frames/test_frame_1.py
+++ tests/device/frames/test_frame_1.py
@@ -10,6 +10,7 @@ from sml2mqtt.config.device import SmlDeviceConfig
 async def test_frame_no_match_obis_id(device: TestingDevice, no_serial, caplog, monkeypatch,
                                       sml_frame_1: SmlFrame, sml_frame_1_analyze, arg_analyze):
     caplog.set_level(logging.DEBUG)
+    monkeypatch.setattr(CONFIG.general, 'device_id_obis', ['0100000009ff', '01006001ffff'])
 
     device.testing_raise_on_status = False
     device.serial_data_read(sml_frame_1)
@@ -17,7 +18,7 @@ async def test_frame_no_match_obis_id(device: TestingDevice, no_serial, caplog, 
     msg = "\n".join(x.msg for x in caplog.records)
 
     assert msg == sml_frame_1_analyze + """
-Found none of the following obis ids in the sml frame: 0100000009ff, 0000600100ff, 0100000001ff, 0000600101ff
+Found none of the following obis ids in the sml frame: 0100000009ff, 01006001ffff
 Received Frame
  -> b'760500531efa620062007263010176010105001bb4fe0b0a0149534b0005020de272620165001bb32e620163a71400760500531efb620062007263070177010b0a0149534b0005020de2070100620affff72620165001bb32e757707010060320101010101010449534b0177070100600100ff010101010b0a0149534b0005020de20177070100010800ff65001c010401621e52ff650026bea90177070100020800ff0101621e52ff62000177070100100700ff0101621b52005301100101016350ba00760500531efc6200620072630201710163ba1900'
 
